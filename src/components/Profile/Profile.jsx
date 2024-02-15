7c232ae6e68067f9a581d3030045ed7f01a994e1
Olya <116801530+marshennikovaolga@users.noreply.github.com>
2024-02-15 14:08:00 +0300

    error logic edit profile data

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,48 +10,63 @@ function Profile({ logOut, updateUserProfile, isEdit, setIsEdit }) {
     const { error: validationError, values, reset, isInputValid, isValidButton, handleChange }
     = useFormValidation({ name: currentUser.name || '', email: currentUser.email || '' });
     const [updateSuccessMessage, setUpdateSuccessMessage] = useState('');
-    const [showCurrentDataError, setShowCurrentDataError] = useState(false);
+    const [initialValues, setInitialValues] = useState({ name: currentUser.name || '', email: currentUser.email || '' });
     const [isDataChanged, setIsDataChanged] = useState(false);
+    const [showCurrentDataError, setShowCurrentDataError] = useState(false);
 
     useEffect(() => {
-        setIsDataChanged(values.name !== currentUser.name || values.email !== currentUser.email);
-    }, [values, currentUser]);
+        setIsDataChanged(values.name !== initialValues.name || values.email !== initialValues.email);
+    }, [values, initialValues]);
 
     useEffect(() => {
-        setShowCurrentDataError(false);
-    }, [isEdit]);
-
-    function handleSubmit(evt) {
-        evt.preventDefault();
-        if (isDataChanged) {
-            updateUserProfile(values.name, values.email);
-            setUpdateSuccessMessage('Данные профиля успешно обновлены');
-        } else {
-            setShowCurrentDataError(true);
+        if (isEdit) {
+            setInitialValues({ name: currentUser.name, email: currentUser.email  });
         }
-        setIsEdit(false);
-    }
+    }, [isEdit, currentUser]);
 
     function handleEditProfile() {
-        setIsEdit(true);
-        setUpdateSuccessMessage('');
         reset();
+        setIsEdit(true);
     }
 
     function handleCancelEdit() {
         reset();
         setIsEdit(false);
-        setUpdateSuccessMessage('');
+        setUpdateSuccessMessage('Редактирование отменено');
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        if (isEdit) {
+            if (values.name !== initialValues.name || values.email !== initialValues.email) {
+                updateUserProfile(values.name, values.email);
+                setUpdateSuccessMessage('Данные профиля успешно обновлены');
+            } else {
+                setUpdateSuccessMessage('Редактирование отменено');
+                setShowCurrentDataError(true);
+            }
+            setIsEdit(false);
+        }
     }
 
     useEffect(() => {
         if (updateSuccessMessage) {
             const timer = setTimeout(() => {
                 setUpdateSuccessMessage('');
-            }, 30000);
+            }, 3000);
             return () => clearTimeout(timer);
         }
     }, [updateSuccessMessage]);
+
+    useEffect(() => {
+        if (showCurrentDataError) {
+            const timer = setTimeout(() => {
+                setShowCurrentDataError(false);
+            }, 2000);
+    
+            return () => clearTimeout(timer);
+        }
+    }, [showCurrentDataError]);
 
     return (
         <form className="profile" onSubmit={handleSubmit} onInvalid={(e) => { e.preventDefault(); }}>
@@ -59,7 +74,7 @@ function Profile({ logOut, updateUserProfile, isEdit, setIsEdit }) {
             {updateSuccessMessage && (
                 <p className="profile_success">{updateSuccessMessage}</p>
             )}
-            {showCurrentDataError && (
+                         {showCurrentDataError && (
                 <p className="profile_data-error">Вы ввели текущие данные</p>
             )}
             <div className='profile__name'>
@@ -128,4 +143,3 @@ function Profile({ logOut, updateUserProfile, isEdit, setIsEdit }) {
 }
 
 export default Profile;
-
