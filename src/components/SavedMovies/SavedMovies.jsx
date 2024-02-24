f3492330601d7f067a1a353c1bd649aec45ab137
Olya <116801530+marshennikovaolga@users.noreply.github.com>
2024-02-24 16:49:14 +0300

    searching

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -3,35 +3,39 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchFormSavedMovies from '../SearchFormSavedMovies/SearchFormSavedMovies';
 import { SHORT_MOVIE_DURATION } from '../../utils/constants';
 
-export default function SavedMovies({ onDelete, savedMovies, movies }) {
+export default function SavedMovies({ onDelete, savedMovies }) {
   const [searchedSaveMovie, setSearchedSaveMovie] = useState('');
   const [isCheck, setIsCheck] = useState(false);
   const [filteredSavedMovies, setFilteredSavedMovies] = useState(savedMovies);
   const [firstSearch, setFirstSearch] = useState(true);
+  const [globalError, setGlobalError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const filterSavedMovies = useCallback((search, isChecked, movies) => {
     setSearchedSaveMovie(search);
     setIsCheck(isChecked);
     if (movies) {
-      setFilteredSavedMovies(
-        movies.filter((movie) => {
-          const searchName = typeof search === 'string' && movie.nameRU.toLowerCase().includes(search.toLowerCase());
-          const durationCheck = !isChecked || movie.duration <= SHORT_MOVIE_DURATION;
-          return searchName && durationCheck;
-        })
-      );
+      const filteredMovies = movies.filter(movie => {
+        const searchName =
+          typeof search === 'string' &&
+          movie.nameRU.toLowerCase().includes(search.toLowerCase());
+        const durationCheck = !isChecked || movie.duration <= SHORT_MOVIE_DURATION;
+        return searchName && durationCheck;
+      });
+      setFilteredSavedMovies(filteredMovies);
+      setGlobalError(false);
+      setIsLoading(false);
     }
   }, []);
 
   useEffect(() => {
     filterSavedMovies(searchedSaveMovie, isCheck, savedMovies);
-  }, [savedMovies, searchedSaveMovie, isCheck, filterSavedMovies]);
+  }, [searchedSaveMovie, isCheck, savedMovies, filterSavedMovies]);
 
   return (
     <>
       <SearchFormSavedMovies
         savedMovies={filteredSavedMovies}
-        movies={movies}
         isCheck={isCheck}
         setIsCheck={setIsCheck}
         searchSavedMovies={filterSavedMovies}
@@ -39,84 +43,13 @@ export default function SavedMovies({ onDelete, savedMovies, movies }) {
         firstSearch={firstSearch}
       />
       <MoviesCardList
-        movies={filteredSavedMovies}
+        movies={filteredSavedMovies} // - фильмы в результате поиска
         onDelete={onDelete}
         savedMovies={savedMovies}
+        initialSearch={firstSearch}
+        globalError={globalError}
+        isLoading={isLoading}
       />
     </>
   );
 }
-
-//         // savedMovies={savedMovies}
-//         // movies={filteredSavedMovies}
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useEffect, useState } from 'react';
-// import MoviesCardList from '../MoviesCardList/MoviesCardList';
-// import SearchFormSavedMovies from '../SearchFormSavedMovies/SearchFormSavedMovies';
-// import { SHORT_MOVIE_DURATION } from '../../utils/constants';
-
-// function SavedMovies({ onDelete, savedMovies }) {
-//   const [globError, setGlobError] = useState(false);
-//   const [isSavedLoading, setIsSavedLoading] = useState(false);
-//   const [searchedSaveMovie, setSearchedSaveMovie] = useState('');
-//   const [filteredSavedMovies, setFilteredSavedMovies] = useState(savedMovies);
-//   const [isCheck, setIsCheck] = useState(false);
-//   const [firstSearch, setFirstSearch] = useState(true);
-
-//   const searchSavedMovies = () => {
-//     const filteredSavedMovies = savedMovies.filter(movie => {
-//       const matchesSearch = movie.nameRU.toLowerCase().includes(searchedSaveMovie.toLowerCase());
-//       return matchesSearch;
-//     });
-//     setFilteredSavedMovies(filteredSavedMovies);
-//   };
-
-//   useEffect(() => {
-//     setIsSavedLoading(true);
-//     setFilteredSavedMovies(savedMovies);
-//     setIsSavedLoading(false);
-//   }, [savedMovies]);
-
-//   const toggleShorts = () => {
-//     setIsCheck(!isCheck);
-//     const filteredSavedMovies = savedMovies.filter(movie => {
-//       const matchesSearch = movie.nameRU.toLowerCase().includes(searchedSaveMovie.toLowerCase());
-//       const matchesDuration = !isCheck ? true : movie.duration <= SHORT_MOVIE_DURATION;
-//       return matchesSearch && matchesDuration;
-//     });
-//     setFilteredSavedMovies(filteredSavedMovies);
-//   };
-
-//   return (
-//     <>
-//       <SearchFormSavedMovies
-//         isCheck={isCheck}
-//         setIsCheck={toggleShorts}
-//         searchSavedMovies={searchSavedMovies}
-//         searchedSaveMovie={searchedSaveMovie}
-//         firstSearch={firstSearch}
-//       />
-//       <MoviesCardList
-//         movies={filteredSavedMovies}
-//         firstSearch={firstSearch}
-//         onDelete={onDelete}
-//         savedMovies={savedMovies}
-//         isLoading={isSavedLoading}
-//         globalError={globError}
-//       />
-//     </>
-//   );
-// };
-
-// export default SavedMovies;
--- src/components/SearchFormSavedMovies/SearchFormSavedMovies.jsx
+++ src/components/SearchFormSavedMovies/SearchFormSavedMovies.jsx
@@ -7,37 +7,34 @@ function SearchFormSavedMovies({
   isCheck,
   setIsCheck,
   searchSavedMovies,
-  searchedSaveMovie,
-  firstSearch,
-  movies
+  firstSearch
 }) {
+  const [searchValue, setSearchValue] = useState('');
   const [errorSaveMessage, setErrorSaveMessage] = useState('');
   const { values, handleChange, reset, isValidButton } = useFormValidation({
     search: ''
   });
 
   useEffect(() => {
-    reset({ search: searchedSaveMovie });
-  }, [searchedSaveMovie, reset]);
+    reset({ search: searchValue });
+  }, [searchValue, reset]);
 
   function handleSubmit(evt) {
     evt.preventDefault();
     const searchSaveValue = values.search.trim();
     if (isValidButton) {
-      searchSavedMovies(searchSaveValue);
+      searchSavedMovies(searchSaveValue, isCheck);
       setErrorSaveMessage('');
     } else {
-      setErrorSaveMessage('Введите ключевое слово для поиска');
+      setErrorSaveMessage('Нужно ввести ключевое слово');
     }
   }
 
   function toggleCheckbox() {
-    setIsCheck(!isCheck);
+    const newSaveShortsState = !isCheck;
+    setIsCheck(newSaveShortsState);
+    localStorage.setItem('lastCheckboxState', newSaveShortsState);
   }
-
-  useEffect(() => {
-    reset({ search: searchedSaveMovie || '' });
-  }, [searchedSaveMovie, reset]);
 
   return (
     <section className="savesearch">
@@ -60,9 +57,8 @@ function SearchFormSavedMovies({
         </form>
         <FilterCheckbox
           isChecked={isCheck}
-          // setIsChecked={toggleCheckbox}
-          setIsChecked={setIsCheck}
           showShorts={toggleCheckbox}
+          setIsChecked={setIsCheck}
           initialSearch={firstSearch}
         />
       </div>
@@ -71,174 +67,3 @@ function SearchFormSavedMovies({
 }
 
 export default SearchFormSavedMovies;
-
-
-
-
-
-
-
-
-
-
-// import { useEffect, useState } from 'react'
-// import useFormValidation from '../../hooks/useFormValidation'
-// import FilterCheckbox from '../FilterCheckbox/FilterCheckbox'
-// import './SearchFormSavedMovies.css'
-
-// export default function SearchFormSavedMovies({
-//   initialSearch,
-//   isChecked,
-//   setIsChecked,
-//   searchMovies,
-//   searchSavedMovies
-// }) {
-//   const { values, error, handleChange, reset, isValidButton } = useFormValidation({ search: '' });
-//   const [errorMessage, setErrorMessage] = useState('');
-
-//   useEffect(() => {
-//     const lastSearch = localStorage.getItem('lastSavedSearch');
-//     const lastCheckboxState = localStorage.getItem('lastSavedCheckboxState');
-//     if (lastSearch) {
-//       reset({ search: lastSearch });
-//     }
-//   }, [reset]);
-
-//   function onSubmit(evt) {
-//     evt.preventDefault();
-//     const searchValue = values.search.trim();
-//     if (isValidButton) {
-//       if (searchSavedMovies) {
-//         searchSavedMovies(searchValue);
-//       }
-//       localStorage.setItem('lastSavedSearch', searchValue);
-//       localStorage.setItem('lastSavedCheckboxState', isChecked);
-//       setErrorMessage('');
-//     } else {
-//       setErrorMessage('Нужно ввести ключевое слово');
-//       setTimeout(() => {
-//         setErrorMessage('');
-//       }, 1500);
-//     }
-//   }
-
-//   function showShorts() {
-//     const newShortsState = !isChecked;
-//     setIsChecked(newShortsState);
-//     localStorage.setItem('lastSavedCheckboxState', newShortsState);
-//   }
-
-//   return (
-//     <section className="savesearch">
-//       <div className="savesearch__container">
-//         {errorMessage && <p className='savesearch__error_message'>{errorMessage}</p>}
-//         <form className="savesearch__form" noValidate onSubmit={onSubmit}>
-//           <input
-//             required
-//             className='search__input'
-//             placeholder="Фильм"
-//             name="search"
-//             type="text"
-//             value={values.search}
-//             onChange={handleChange}
-//             error={error.text}
-//             autoComplete="off"
-//           />
-//           <button
-//             type='submit'
-//             className='savesearch__submit'
-//           ></button>
-//         </form>
-//         <FilterCheckbox
-//           isChecked={isChecked}
-//           setIsChecked={setIsChecked}
-//           showShorts={showShorts}
-//           initialSearch={initialSearch}
-//         />
-//       </div>
-//     </section>
-//   );
-// }
-
-
-
-
-
-
-// import { useEffect, useState } from 'react';
-// import useFormValidation from '../../hooks/useFormValidation';
-// import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-// import './SearchFormSavedMovies.css';
-
-// export default function SearchFormSavedMovies({
-//   initialSearch,
-//   isChecked,
-//   setIsChecked,
-//   searchSavedMovies,
-// }) {
-//   const { values, error, handleChange, reset, isValidButton } = useFormValidation({ search: '' });
-//   const [errorMessage, setErrorMessage] = useState('');
-
-//   useEffect(() => {
-//     const lastSearch = localStorage.getItem('lastSavedSearch');
-//     const lastCheckboxState = localStorage.getItem('lastSavedCheckboxState');
-//     if (lastSearch) {
-//       reset({ search: lastSearch });
-//     }
-//   }, [reset]);
-
-//   function onSubmit(evt) {
-//     evt.preventDefault();
-//     const searchValue = values.search.trim();
-//     if (isValidButton) {
-//       if (searchSavedMovies) {
-//         searchSavedMovies(searchValue);
-//       }
-//       localStorage.setItem('lastSavedSearch', searchValue);
-//       localStorage.setItem('lastSavedCheckboxState', isChecked);
-//       setErrorMessage('');
-//     } else {
-//       setErrorMessage('Нужно ввести ключевое слово');
-//       setTimeout(() => {
-//         setErrorMessage('');
-//       }, 1500);
-//     }
-//   }
-
-//   function showShorts() {
-//     const newShortsState = !isChecked;
-//     setIsChecked(newShortsState);
-//     localStorage.setItem('lastSavedCheckboxState', newShortsState);
-//   }
-
-//   return (
-//     <section className="savesearch">
-//       <div className="savesearch__container">
-//         {errorMessage && <p className='savesearch__error_message'>{errorMessage}</p>}
-//         <form className="savesearch__form" noValidate onSubmit={onSubmit}>
-//           <input
-//             required
-//             className='savesearch__input'
-//             placeholder="Фильм"
-//             name="search"
-//             type="text"
-//             value={values.search}
-//             onChange={handleChange}
-//             error={error.text}
-//             autoComplete="off"
-//           />
-//           <button
-//             type='submit'
-//             className='savesearch__submit'
-//           ></button>
-//         </form>
-//         <FilterCheckbox
-//           isChecked={isChecked}
-//           setIsChecked={setIsChecked}
-//           showShorts={showShorts}
-//           initialSearch={initialSearch}
-//         />
-//       </div>
-//     </section>
-//   );
-// }
